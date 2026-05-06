52f063d228d362bcc2dc7c3a4aca940126ab7adc
kimhyunjin <kimkongpp1223@gmail.com>
2026-05-06 16:10:07 +0900

    Service 생성

---
+++ src/main/java/com/codehideout/service/PostService.java
@@ -0,0 +1,19 @@
+package com.codehideout.service;
+
+import com.codehideout.entity.Post;
+import com.codehideout.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
+}
