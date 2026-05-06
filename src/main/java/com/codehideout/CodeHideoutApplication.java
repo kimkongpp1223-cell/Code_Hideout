ef8a4b61a97126cea1712140c4f474922536923b
kimhyunjin <kimkongpp1223@gmail.com>
2026-05-06 14:27:57 +0900

    개발자커뮤니티 시작 : DB 설정 완료

---
+++ src/main/java/com/codehideout/CodeHideoutApplication.java
@@ -0,0 +1,13 @@
+package com.codehideout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeHideoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeHideoutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codehideout/CodeHideoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehideout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeHideoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
