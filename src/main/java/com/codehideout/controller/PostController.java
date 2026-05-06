f254adeaf4b3c03e0b1c7e7ad1d904b590300553
kimhyunjin <kimkongpp1223@gmail.com>
2026-05-06 16:29:11 +0900

    PostController 생성,list페이지 생성

---
+++ src/main/java/com/codehideout/controller/PostController.java
@@ -0,0 +1,22 @@
+package com.codehideout.controller;
+
+import com.codehideout.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping("/posts")
+    public String postList(Model model) {
+
+        model.addAttribute("posts", postService.findAll());
+
+        return "post/list";
+    }
+}
