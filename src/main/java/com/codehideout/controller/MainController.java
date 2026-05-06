3dcf676112699373585c669324d1a38925506cf2
kimhyunjin <kimkongpp1223@gmail.com>
2026-05-06 15:27:20 +0900

    entity post 생성

--- src/main/java/com/codehideout/controller/MainController.java
+++ src/main/java/com/codehideout/controller/MainController.java
@@ -1,13 +1,18 @@
 package com.codehideout.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+
+        model.addAttribute("siteName","Code Hideout");
+        model.addAttribute("message","개발자 커뮤니티 사이트 입니다.");
+
         return "index";
     }
 }
---
+++ src/main/java/com/codehideout/entity/Post.java
@@ -0,0 +1,25 @@
+package com.codehideout.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private String writer;
+
+    private LocalDateTime createdAt;
+}
